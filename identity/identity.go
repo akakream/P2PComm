8aca503ef248ffb279b0c2c503c7bdb79fd16b4e
akakream <akakream@gmail.com>
2023-06-03 23:29:55 +0200

    [WIP] identity is being added

---
+++ identity/identity.go
@@ -0,0 +1,56 @@
+package identity
+
+import (
+	"crypto/rand"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	crypto "github.com/libp2p/go-libp2p/core/crypto"
+	peer "github.com/libp2p/go-libp2p/core/peer"
+)
+
+type Identity struct {
+	KeyPath string
+	PrivKey crypto.PrivKey
+	PeerID  peer.ID
+}
+
+func NewIdentity(datapath string) (*Identity, error) {
+	privateKeyPath := filepath.Join(datapath, "privateKey")
+	var privKey crypto.PrivKey
+
+	_, err := os.Stat(privateKeyPath)
+	if os.IsNotExist(err) {
+		privKey, _, err = crypto.GenerateRSAKeyPair(2048, rand.Reader)
+		if err != nil {
+			return nil, err
+		}
+		privKeyBytes, err := crypto.MarshalPrivateKey(privKey)
+		if err != nil {
+			return nil, err
+		}
+		err = ioutil.WriteFile(privateKeyPath, privKeyBytes, 0400)
+		if err != nil {
+			return nil, err
+		}
+	} else if err != nil {
+		return nil, err
+	} else {
+		key, err := ioutil.ReadFile(privateKeyPath)
+		if err != nil {
+			return nil, err
+		}
+		privKey, err = crypto.UnmarshalPrivateKey(key)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	pid, err := peer.IDFromPublicKey(privKey.GetPublic())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Identity{KeyPath: privateKeyPath, PrivKey: privKey, PeerID: pid}, err
+}
